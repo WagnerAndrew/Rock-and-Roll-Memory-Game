9e37f5d7bfd1ce1aaddd19b1f7ac329d4fd04748
WagnerAndy <wagner.andrew.james@gmail.com>
2019-01-25 15:39:55 -0700

    first commit

---
+++ src/components/Title/index.js
@@ -0,0 +1,15 @@
+
+import React from "react";
+import "./style.css";
+
+function Title(props) {
+  return <header>
+      
+        <h1>Click a Rock & Roll Hall of Famer to begin... But don't click them twice!</h1>
+        <h2>Score: 0 | Top Score: 0</h2>
+    
+    </header>;
+
+}
+
+export default Title;
