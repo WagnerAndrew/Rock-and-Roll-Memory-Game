5db7246f0472590c96f5f9b78b4145b148e8f098
WagnerAndy <wagner.andrew.james@gmail.com>
2019-01-25 15:57:53 -0700

    Updates

---
+++ precache-manifest.f47b52e60d6d7ada0d2388bf712b5789.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e758fd0afc9f43e7a85",
+    "url": "/[your-user-name].github.io/WagnerAndy/click-mem-game/static/js/runtime~main.4e758fd0.js"
+  },
+  {
+    "revision": "3ca152ad8a127fe2efd0",
+    "url": "/[your-user-name].github.io/WagnerAndy/click-mem-game/static/js/main.3ca152ad.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/[your-user-name].github.io/WagnerAndy/click-mem-game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3ca152ad8a127fe2efd0",
+    "url": "/[your-user-name].github.io/WagnerAndy/click-mem-game/static/css/main.f06f07a2.chunk.css"
+  },
+  {
+    "revision": "ffcc1484aa362c5f918cbfc83e34ecca",
+    "url": "/[your-user-name].github.io/WagnerAndy/click-mem-game/index.html"
+  }
+];
